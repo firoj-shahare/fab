2b5b741b53eaa93798bb2ca85e990a9dde3bb210
firoj-shahare <firoj.ndsoftware@gmail.com>
2023-07-26 22:11:09 +0530

    First Commit.

---
+++ src/test/java/com/practice/git/GitPracticeApplicationTests.java
@@ -0,0 +1,14 @@
+package com.practice.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+          public String abd="str";
+	}
+
+}
