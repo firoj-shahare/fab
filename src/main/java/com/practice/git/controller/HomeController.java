db27fcfaa73e9388dac4e696450271d1fd6cedba
firoj-shahare <firoj.ndsoftware@gmail.com>
2023-07-26 23:13:16 +0530

    "New Login and Home controller created to check if the pull request is open in the first banch will reflect or not."

---
+++ src/main/java/com/practice/git/controller/HomeController.java
@@ -0,0 +1,9 @@
+package com.practice.git.controller;
+
+public class HomeController {
+
+	public HomeController() {		
+		System.out.println(" Home controller created in branch 2");
+	}
+
+}
---
+++ src/main/java/com/practice/git/controller/LoginController.java
@@ -0,0 +1,9 @@
+package com.practice.git.controller;
+
+public class LoginController {
+
+	public LoginController() {
+		System.out.println("Login Controller is created in Branch 2 also...");
+	}
+
+}
