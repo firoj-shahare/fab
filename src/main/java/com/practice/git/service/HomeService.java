db1be02905d0351909a2fa1a65d77b673bac36e5
firoj-shahare <firoj.ndsoftware@gmail.com>
2023-07-27 09:13:38 +0530

    "First Commit in Branch 3 "

---
+++ src/main/java/com/practice/git/service/HomeService.java
@@ -0,0 +1,14 @@
+package com.practice.git.service;
+
+public class HomeService {
+
+	public HomeService() {
+
+		System.out.println("****** Home Service *******");
+
+		System.out.println("Home Service Created for Branch 3");
+		System.out.println("To check if the Pull Request Open for Branch 1");
+		System.out.println("Will the commit og branch 3 is reflected in the Branch 1 Pull Request.");
+	}
+
+}
